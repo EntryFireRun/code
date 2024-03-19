4445e17cbbd08cfb441c4b5fe8a68b48427a2f7e
FireRun <entryfirerun7012@outlook.com>
2024-03-19 22:26:42 +0900

    Create 4.js

---
+++ 4.js
@@ -0,0 +1,53 @@
+const EhdlqfurgkwlaktpdyWW = 1;
+if (typeof EhdlqfurgkwlaktpdyWW == "number") {
+  let wasans = 1;
+  Entry.engine.speeds[5] = 300; // undefind나 null등으로 적은 경우, 작품 상세 페이지에서 64fps로 동작하기 때문에 300으로 변경 (테스트 결과 250 이상은 '그 방법' 없이는 못 넘는 듯)
+  document.getElementsByClassName("entrySpeedButtonWorkspace")[0].click();
+  document
+    .getElementsByClassName("entrySpeedButtonWorkspace")[0]
+    .addEventListener("click", () => {
+      if (
+        document
+          .getElementsByClassName("entrySpeedButtonWorkspace")[0]
+          .classList.contains("on")
+      ) {
+        document.getElementById("progressCell5").style.backgroundColor =
+          "blue";
+        document
+          .getElementById("progressCell5")
+          .addEventListener("click", () => {
+            document.getElementById("progressCell4").classList.remove("on");
+            document.getElementById("progressCell3").classList.remove("on");
+            document.getElementById("progressCell2").classList.remove("on");
+            document.getElementById("progressCell1").classList.remove("on");
+            document.getElementById("progressCell5").classList.add("on");
+            wasans = 0;
+          });
+        document
+          .getElementById("progressCell4")
+          .addEventListener("click", () => {
+            wasans = 1;
+          });
+        document
+          .getElementById("progressCell3")
+          .addEventListener("click", () => {
+            wasans = 1;
+          });
+        document
+          .getElementById("progressCell2")
+          .addEventListener("click", () => {
+            wasans = 1;
+          });
+        document
+          .getElementById("progressCell1")
+          .addEventListener("click", () => {
+            wasans = 1;
+          });
+        if (wasans == 0) {
+          document.getElementById("progressCell5").click();
+        }
+      }
+    });
+  document.getElementsByClassName("entrySpeedButtonWorkspace")[0].click();
+}
+// 코드 야매로 구현함ㅋ
