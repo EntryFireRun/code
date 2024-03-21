b720e404b88564116ec2ef8d6840b058d31bc255
FireRun <entryfirerun7012@outlook.com>
2024-03-21 20:21:36 +0900

    Update afreecaTV_VOD_download.py

--- afreecaTV_VOD_download.py
+++ afreecaTV_VOD_download.py
@@ -6,8 +6,6 @@
 # 중지는 Ctrl Z나 Ctrl C 로 가능합니다
 # 새 폴더에서 실행해주세요, 파일 이름 겹치면 파일을 잃거나 고장날 수 있습니다
 # 지원하는 VOD : 유저VOD, 업로드VOD, 유저 클립, 하일라이트, 캐치
-# 지원 해야할 것 : https://vod-archive-kr-cdn-z01.afreecatv.com/v101/hls/highlight/20160822/207/bd72bbaa_180467207_1_2_A.mp4/original/both/seg-15.ts 하일라이트 https://vod.afreecatv.com/player/12047656
-# 캐시 : https://vod.afreecatv.com/player/116049503/catch https://vod-normal-kr-cdn-z01.afreecatv.com/v101/hls/clip/20240212/145/da9a8bc0-c7f8-432f-aa14-449079bdeb12/3164145.smil/original/both/seg-2.ts
 # 다시보기는 용량 문제로 지원하지 않을 예정 
 
 import urllib.request
