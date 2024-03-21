66bf256564eaa219d69b1fadbfbe15dde3004c1a
FireRun <entryfirerun7012@outlook.com>
2024-03-21 17:44:06 +0900

    Update afreecatvVODdownload.py

--- afreecatvVODdownload.py
+++ afreecatvVODdownload.py
@@ -1,13 +1,10 @@
 # 실행을 위해 다음을 설치해야 합니다
-# FFmpeg, python os 모듈, python requests 모듈, python requests 모듈(스파게티 코드로 짜면서 나온건데, 이거 필요 없을 수도 있음), python BeautifulSoup 모듈,
+# FFmpeg, python os 모듈, python requests 모듈, python requests 모듈(스파게티 코드로 짜면서 나온건데, 이거 필요 없을 수도 있음), python BeautifulSoup 모듈
 # (추가로 모듈을 설치하기 위해 pip 필요), (해당 코드를 실행하기 위해 python 필요)
 # 해당 코드는 윈도우에 맞춰져있습니다, 윈도우로 실행해주세요, 오류나거나, 만들어진 파일 안 지워지거나, 파일이 안합쳐질 수도 있습니다
 # 아프리카 tv가 업데이트 되면 제대로 실행이 안될 수 있습니다, 업데이트로 문제가 생기면 계속 계산하지 않고, 코드를 끝내도록 설계되었으나, 그것도 오류가 날 수 있으니, 이상하다 싶으면 알아서 끄세요
-# 중지는 Ctrl Z나 Ctrl C 로 가능할거예요
-# 아 맞다, 이거 새 폴더에서 실행해주세요, 파일 이름 겹치면 파일을 잃거나 고장날 수 있습니다
-# 만든 이유는, yt-dlp로 아프리카tv VOD저장 고장나서 만든거예요
-# 레포가 entrycode지만 엔트리와 관련 없고 그냥 올리는거
-# 몇 몇 영상 다운 안되는데 고쳐가는 중
+# 중지는 Ctrl Z나 Ctrl C 로 가능합니다
+# 새 폴더에서 실행해주세요, 파일 이름 겹치면 파일을 잃거나 고장날 수 있습니다
 
 import urllib.request
 import os
@@ -15,53 +12,71 @@ import requests
 from bs4 import BeautifulSoup
 
 def save_video(video_url, a) :
-    save_name = f"chunk-{a}.ts"
-    urllib.request.urlretrieve(video_url, save_name)
-    file = open('file.txt', "a")
-    file.write(f"file 'chunk-{a}.ts'\n")
-    file.close()
-    print(f"저장됨 : {save_name} / chunk-{dsans}.ts")
-isnumber = input("아프리카tv VOD 저장 프로그램입니다!\nVOD URL : ")
-wtfbro = input("저장할 파일 명 (확장자는 자동으로 mp4가 적용됩니다): ")
+    global gsans
+    try:
+        save_name = f"chunk-{a}.ts"
+        urllib.request.urlretrieve(video_url, save_name)
+        file = open('chunk-l.txt', "a")
+        file.write(f"file 'chunk-{a}.ts'\n")
+        file.close()
+        gsans += os.path.getsize(save_name)
+        print(f"저장됨 : {save_name} / chunk-{dsans}.ts | 예상 용량 : {dsans*3}mb | 현재 용량 : {int(gsans/(1024*1024))}mb",end='\r')
+    except:
+        print("프로그램에 오류가 있어, 파일을 정리한 후 프로그램이 종료됩니다")
+        os.remove("chunk-l.txt")
+        for i in removeList:
+            os.remove(i)
 
-file = open('file.txt', "w")
+isnumber = input("아프리카tv VOD 저장 프로그램입니다!\nVOD URL : ")
+wtfbro = input("저장할 파일 명 (확장자는 자동으로 mp4가 적용됩니다, 파일이 이미 있는 경우 제거됩니다) : ")
+file = open('chunk-l.txt', "w")
 file.write("")
 file.close()
 
 response = requests.get(isnumber)
 soup = BeautifulSoup(response.text, "html.parser")
 div_tag = soup.find_all('meta')
-asans = str(div_tag).split("videoimg")[1].split("og:image")[0].split("evod2_")[1].split("_")[0]
-bsans = str(div_tag).split("videoimg")[1].split("og:image")[0].split("rowKey=")[1].split("_")[0]
-csans = f"https://vod-archive-kr-cdn-z01.afreecatv.com/v101/hls/evod/{bsans}/{asans}/{bsans}_evod2_{asans}.smil/original/both/"
-dsans = 100 # 처음부터 계산하면 너무 느리고, 404는 데이터 받는게 빠르게 때문에 효율적인 알고리즘 사용
+try:
+    asans = str(div_tag).split("videoimg")[1].split("og:image")[0].split("evod2_")[1].split("_")[0]
+    bsans = str(div_tag).split("videoimg")[1].split("og:image")[0].split("rowKey=")[1].split("_")[0]
+    csans = f"https://vod-archive-kr-cdn-z01.afreecatv.com/v101/hls/evod/{bsans}/{asans}/{bsans}_evod2_{asans}.smil/original/both/"
+except:
+    asans = str(div_tag).split("save/afreeca/station/")[1].split(".")[0].split("_")[0].split("/thumb/")[0]
+    bsans = str(div_tag).split("save/afreeca/station/")[1].split("/thumb/")[1].split("_")[0]
+    csans = f"https://vod-normal-kr-cdn-z01.afreecatv.com/v101/hls/save/afreeca/station/{asans}/{bsans}.smil/original/both/"
+dsans = 70 # 처음부터 계산하면 너무 느리고, 404는 데이터 받는게 빠르게 때문에 70청크 부터 계산하는 효율적인 알고리즘 사용
 esans = 0
+fsans = 0
 while True:
-    if requests.get(f"https://vod-archive-kr-cdn-z01.afreecatv.com/v101/hls/evod/{bsans}/{asans}/{bsans}_evod2_{asans}.smil/original/both/seg-{dsans}.ts").status_code == 200:
-        if dsans >= 100:
-            dsans += 10
+    if requests.get(f"{csans}seg-{dsans}.ts").status_code == 200:
+        if dsans >= 70:
             if esans == 1:
                 break
+            else:
+                dsans += 10
+                print(f"영상 청크 검색 중... / 현재 검색된 개수 : {dsans}",end='\r')
         else:
             break
     else:
         dsans -= 1
         esans = 1
-        print(f"영상 청크 검색 중... / 현재 검색된 개수 : {dsans}")
+        print(f"영상 청크 검색 중... / 현재 검색된 개수 : {dsans}",end='\r')
         if dsans == 0:
-            print("오류 : 검색 인덱스 범위 초과\n님 이상한 url 적으셨죠;")
+            print("\n오류 : 검색 인덱스 범위 초과 : URL을 다시 확인해주세요,",end='\r')
             quit()
 removeList = []
+gsans = 0
+print()
 if os.path.isfile(f"{wtfbro}.mp4"):
     os.remove(f"{wtfbro}.mp4")
 for i in range(int(dsans) + 1):
     save_video(f"{csans}seg-{i}.ts", i)
     removeList.append(f"chunk-{i}.ts")
-
-os.system(f"ffmpeg -f concat -i file.txt -c copy {wtfbro}.mp4")
-os.remove("file.txt")
+os.system(f"ffmpeg -f concat -i chunk-l.txt -c copy {wtfbro}.mp4")
+os.remove("chunk-l.txt")
 for i in removeList:
     os.remove(i)
 os.system('cls')
-print(f"\"{wtfbro}\"파일이 생성되었습니다, 제대로 생성되지 못한 경우, 다시 시도해주세요")
+print(f"\"{wtfbro}.mp4\"파일이 생성되었습니다, 제대로 생성되지 못한 경우, 다시 시도해주세요")
 # https://vod.afreecatv.com/player/119563493 <== test URL
+# https://vod.afreecatv.com/player/119194463 <== test URL
