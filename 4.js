e1992c63bf40937dcbe5265794b4ce5dd0e6e32e
FireRun <entryfirerun7012@outlook.com>
2024-03-23 12:48:52 +0900

    Update 4.js

--- 4.js
+++ 4.js
@@ -1,6 +1,9 @@
 const EhdlqfurgkwlaktpdyWW = 1;
 if (typeof EhdlqfurgkwlaktpdyWW == "number") {
   let wasans = 1;
+  if (Entry.FPS == 300) {
+    wasans = 0;
+  }
   Entry.engine.speeds[5] = 300; // undefind나 null등으로 적은 경우, 작품 상세 페이지에서 64fps로 동작하기 때문에 300으로 변경 (테스트 결과 250 이상은 '그 방법' 없이는 못 넘는 듯)
   document.getElementsByClassName("entrySpeedButtonWorkspace")[0].click();
   document
