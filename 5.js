abac1a145531c36e36d47dda314ea813f6b3c81f
FireRun <entryfirerun7012@outlook.com>
2024-03-30 11:28:49 +0900

    Create 5.js

---
+++ 5.js
@@ -0,0 +1,25 @@
+wawasans = prompt("유저 아이디를 입력해주세요")
+wawawasans = new Date(parseInt(wawasans.slice(0, 7), 16)*1000).toString().split(" ")[0].replace("Mon", "월요일").replace("Tue", "화요일").replace("Wed", "수요일").replace("Thu", "목요일").replace("Fri", "금요일").replace("Sat", "토요일").replace("Sun", "일요일");
+function wasans(a) {
+    alert(`${a.split("-")[0]}년 ${a.split("-")[1]}월 ${a.split("-")[2].split("T")[0]}일 ${wawawasans} ${parseInt(a.split("T")[1].split(":")[0])+9}시 ${a.split(":")[1]}분 ${a.split(":")[2].slice(0, -1)}초에 생성된 계정입니다 `)
+}
+
+fetch("https://playentry.org/graphql/FIND_USERSTATUS_BY_USERNAME", {
+      headers: {
+      "content-type": "application/json",
+      "csrf-token": `${
+        JSON.parse(document.getElementById("__NEXT_DATA__").innerText).props
+          .initialProps.csrfToken
+      }`,
+      "x-client-type": "Client",
+      "x-token": `${
+        JSON.parse(document.getElementById("__NEXT_DATA__").innerText).props
+          .initialState.common.user.xToken
+      }`,
+    },
+  referrerPolicy: "unsafe-url",
+  body: `{"query":"query FIND_USERSTATUS_BY_USERNAME($id: String){userstatus(id: $id){created}}","variables":{"id":"${wawasans}"}}`,
+  method: "POST",
+  mode: "cors",
+  credentials: "include",
+}).then((response) => response.json()).then((data) => wasans(data.data.userstatus.created));
