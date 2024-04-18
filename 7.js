89dac1507e38e7148998925b98463b87cc75b502
FireRun <entryfirerun7012@outlook.com>
2024-04-18 23:51:09 +0900

    Create 7.js

---
+++ 7.js
@@ -0,0 +1,8 @@
+let wasans = 0
+    window.addEventListener('scroll', function() {
+      if (document.documentElement.scrollHeight - window.innerHeight - window.scrollY < 1500 && wasans == 0) {
+        document.querySelector('.css-qtq074').click();
+        wasans = 1
+        setTimeout(() => { wasans = 0 }, 500)
+      }
+    });
