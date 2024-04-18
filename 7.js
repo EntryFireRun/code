b5171197ddd1ef11220934cd1b6e90f91b35f4ae
FireRun <entryfirerun7012@outlook.com>
2024-04-18 23:54:47 +0900

    Update 7.js

--- 7.js
+++ 7.js
@@ -1,8 +1,1 @@
-let wasans = 0
-    window.addEventListener('scroll', function() {
-      if (document.documentElement.scrollHeight - window.innerHeight - window.scrollY < 1500 && wasans == 0) {
-        document.querySelector('.css-qtq074').click();
-        wasans = 1
-        setTimeout(() => { wasans = 0 }, 500)
-      }
-    });
+function wasans() { document.querySelector(".css-qtq074").click(); setTimeout(() => { wasans() }, 500) } wasans()
