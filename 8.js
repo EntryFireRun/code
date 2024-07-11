e713460a854690d8b4c5aa121c3a231efeac2e64
FireRun <entryfirerun7012@outlook.com>
2024-07-11 18:20:34 +0900

    Update 8.js

--- 8.js
+++ 8.js
@@ -12,7 +12,7 @@ fetch("https://playentry.org/graphql", {
     }`,
   },
   referrerPolicy: "unsafe-url",
-  body: `{"query":" query SELECT_FOLLOWERS( $user: String ){ followers(user: $user) { list { user { id } } } }","variables":{"user":"${prompt('팔로우를 삭제할 유저의 마이페이지 주소 입력 [최대 10명]').split('/profile/')[1].split('?')[0]}","pageParam":{"display":10}}}`,
+  body: `{"query":" query SELECT_FOLLOWERS( $user: String ){ followers(user: $user) { list { id } } }","variables":{"user":"${prompt('팔로우를 삭제할 유저의 마이페이지 주소 입력\n[최대 8명 그 이상으로 요청을 보내는 경우 429 error를 반환 함]').split('/profile/')[1].split('?')[0]}","pageParam":{"display":10}}}`,
   method: "POST",
   mode: "cors",
   credentials: "include",
@@ -20,7 +20,7 @@ fetch("https://playentry.org/graphql", {
   .then((response) => response.json())
   .then((data) => {
       console.log(data.data.followers.list.length)
-    for (let i = 0; i < data.data.followers.list.length; i++) {
+    for (let i = 0; i < (data.data.followers.list.length > 8 ? 8 : data.data.followers.list.length); i++) {
       fetch("https://playentry.org/graphql", {
         headers: {
           "content-type": "application/json",
@@ -35,7 +35,7 @@ fetch("https://playentry.org/graphql", {
           }`,
         },
         referrerPolicy: "unsafe-url",
-        body: `{"query":" mutation REMOVE_FOLLOW($id: ID!) { removeFollow(id: $id) { result } } ","variables":{"id":"${data.data.followers.list[i].user.id}"}}`,
+        body: `{"query":" mutation REMOVE_FOLLOW($id: ID!) { removeFollow(id: $id) { status } } ","variables":{"id":"${data.data.followers.list[i].id}"}}`,
         method: "POST",
         mode: "cors",
         credentials: "include",
